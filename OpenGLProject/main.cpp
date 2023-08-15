7114ea0c42d2bbd2236cbbe33335949698b6fcc3
Фрим <1_kulak@mail.ru>
2023-08-15 23:49:00 +0300

    Внесение изменений

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -150,7 +150,7 @@ int main(void)
 		int hour = ((int)t / 3600) % 10;
         float minute = fmod(t / 60.0, 60.0);
         float second = fmod(t, 60.0);
-
+		float a = 1;
         drawClock(hour, minute, second, 0.1);
 
 		glfwPollEvents();
